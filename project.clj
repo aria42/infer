fa709bc7177c6df8327b4b5537e4f33f122c0d37
Mark McGranaghan <mmcgrana@gmail.com>
2010-03-15 17:11:56 -0700

    Tweak project.clj.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject infer "1.0-SNAPSHOT"
   :description "inference and machine learning for clojure"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-		 [org.clojure/clojure-contrib "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-master-SNAPSHOT"]
                  [org.apache.commons/commons-math "2.0"]
                  [org.incanter/incanter-core "1.0.0"]
                  [criterium/criterium "0.0.1-SNAPSHOT"]]
