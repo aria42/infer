df871ebe1febe5b3620806e0e2a19eb19eb3793e
Mark McGranaghan <mmcgrana@gmail.com>
2010-03-16 16:43:07 -0700

    Use lein-clojars.

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
                  [org.clojure/clojure-contrib "1.2.0-master-SNAPSHOT"]
                  [org.apache.commons/commons-math "2.0"]
                  [ujmp-complete "0.2.4"]]
-  :dev-dependencies [[swank-clojure "1.1.0-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.1.0-SNAPSHOT"]
+                     [lein-clojars "0.5.0"]]
   :repositories {"incanter" "http://repo.incanter.org"})
