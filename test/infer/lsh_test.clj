5d2c40c4fb63622de3f2b8fa185470faacf234c1
Hamilton <hamilton.ulmer@gmail.com>
2010-07-04 12:09:20 -0700

    Rewrote tests to conform with others in the lib.  ALso added a merging-multiple-tables test.

--- test/infer/lsh_test.clj
+++ test/infer/lsh_test.clj
@@ -1,50 +1,61 @@
 (ns infer.lsh-test
   (:use infer.lsh)
   (:use clojure.test))
-
-(def v1 [5 5 5 5])
-(def permutation-dict-1 {3 0, 2 1, 1 2, 0 3})
-(def permutation-dict-2 {3 2, 2 1, 1 0, 0 3})
-(def sparse-pt-1 #{1 3})
-(def sparse-pt-2 #{3 2})
-(def hamming-pt-1 [0 0 0 1 0 0 0 0 0])
-(def hamming-pt-2 [1 2 10 4 5 6 7 8 9 20])
-(def lp-pt-1 [1 1 1 1 1])
-(def lp-pt-2 [1 1 0 0 0])
-
-(def lsh-table [{} {}])
-(def lsh-hamming-hash-fcns
-  [(hamming-hash 1) (hamming-hash 2)])
-(def hashed-hamming 
-  (map #(% hamming-pt-2) lsh-hamming-hash-fcns))
   
 (deftest basic-dot-product
-  (is (= 100 (dot-product v1 v1))))
+  (let [v [5 5 5 5]]
+  (is (= 100 (dot-product v v)))))
 
 (deftest basic-exact-minhash
-  (is (= 0 ((exact-minhash permutation-dict-1) sparse-pt-1)))
+  (let [perm-dict-1 {3 0, 2 1, 1 2, 0 3}
+	perm-dict-2 {3 2, 2 1, 1 0, 0 3}
+	p1 #{1 3}
+	p2 #{3 2}]
+  (is (= 0 ((exact-minhash perm-dict-1) p1)))
   (is (= [0 1]
        (map
-         #(% sparse-pt-2)
+         #(% p2)
          (map
            exact-minhash 
-           [permutation-dict-1 
-           permutation-dict-2])))))
+           [perm-dict-1 
+            perm-dict-2]))))))
 
 (deftest basic-hamming-hash
-  (is (= 1 ((hamming-hash 3) hamming-pt-1)))
+  (let [v1 [0 0 0 1 0 0 0 0 0]
+	v2 [1 2 10 4 5 6 7 8 9 20]] 
+  (is (= 1 ((hamming-hash 3) v1)))
   (is (= [1 10]
-    (map #(% hamming-pt-2)
-         (map hamming-hash [0 2])))))
+    (map #(% v2)
+         (map hamming-hash [0 2]))))))
 
 (deftest basic-lp-hash
-  (is (= 1 ((lp-hash lp-pt-1 1 5) lp-pt-1)))
+  (let [v1 [1 1 1 1 1] v2 [1 1 0 0 0]]
+  (is (= 1 ((lp-hash v1 1 5) v1)))
   (is (= [1 0]
-      (map #(% lp-pt-1)
-       (map lp-hash
-         [lp-pt-1 lp-pt-2]
-         [1  1]
-         [5 5])))))
+      (map #(% v1)
+       (map lp-hash [v1 v2] [1  1] [5 5]))))))
 
-(deftest add-to-lsh
-  (is (= [{[2] 1} {[10] 1}] (assoc-lsh lsh-table hashed-hamming 1))))
+(deftest add-to-tables
+  (let [v1 [0 0 3 0 0 0 0 0 0]
+	v2 [1 2 3 4 5 6 7 8 9 20]
+	lsh-table [{} {}]
+	hamming-hash-fcns [(hamming-hash 1) (hamming-hash 2)]
+	hp1 (map #(% v1) hamming-hash-fcns)
+	hp2 (map #(% v2) hamming-hash-fcns)]
+    (is (= [{[2] #{1}} {[3] #{1}}]
+	   (assoc-lsh lsh-table hp2 1)))
+    (is (= [{[2] #{2}, [0] #{1}}
+	    {[3] #{1, 2}}]
+	     (assoc-lsh lsh-table hp1 1 hp2 2)
+	     ))))
+
+(deftest merging-multiple-tables
+  (let [table1
+	[{[1 1] #{1 2}, [2 2] #{3 4}}
+	 {[1 2] #{1}, [2 3] #{2 3 4}}]
+	table2
+	[{[1 1] #{5}, [2 3] #{6 7}}
+	 {[1 2] #{6}, [2 4] #{5 7}}]]
+    (is (= [{[1 1] #{1 2 5}, [2 2] #{3 4}, [2 3] #{6 7}}
+	    {[1 2] #{1 6}, [2 3] #{2 3 4}, [2 4] #{5 7}}]
+	     (merge-tables table1 table2)))))
