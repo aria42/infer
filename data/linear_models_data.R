037b75ac27121c1b55d313dd6dc8d06363db7da0
Hamilton <hamilton.ulmer@gmail.com>
2010-07-27 10:14:45 -0700

    Added a script to generate test data.

---
+++ data/linear_models_data.R
@@ -0,0 +1,18 @@
+library(glmnet)
+library(plyr)
+
+lambda <- 0.0315
+
+set.seed(1000)
+
+x <- scale(matrix(rnorm(100*20), 100, 20))
+y <- rnorm(100)
+y <- y - mean(y)
+
+f <- glmnet(x,y, lambda=c(lambda))
+
+coef(f, lambda)
+wr<- testite.table(x, "lars_x.csv", col.names=F, row.names=F, sep=",")
+write.table(y, "lars_y.csv", col.names=F, row.names=F, sep=",")
+write.table(as.matrix(coef(f, lambda)), "lars_betas.csv", col.names=F, row.names=F, sep=",")
+write.table(lambda, "lars_lambda.csv", col.names=F, row.names=F, sep=",")
