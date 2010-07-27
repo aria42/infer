43cf25d911e818d8badcbc53f78b80b6d9b0a2f7
Hamilton <hamilton.ulmer@gmail.com>
2010-07-27 10:54:44 -0700

    Fixed inconsistent test stuff.

--- data/linear_models_data.R
+++ data/linear_models_data.R
@@ -12,7 +12,7 @@ y <- y - mean(y)
 f <- glmnet(x,y, lambda=c(lambda))
 
 coef(f, lambda)
-wr<- testite.table(x, "lars_x.csv", col.names=F, row.names=F, sep=",")
+wr<- write.table(x, "lars_x.csv", col.names=F, row.names=F, sep=",")
 write.table(y, "lars_y.csv", col.names=F, row.names=F, sep=",")
 write.table(as.matrix(coef(f, lambda)), "lars_betas.csv", col.names=F, row.names=F, sep=",")
 write.table(lambda, "lars_lambda.csv", col.names=F, row.names=F, sep=",")
