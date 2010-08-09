c7e209a531298837bb4c22321a968b8e991f7616
Bradford Cross <bradford.n.cross@gmail.com>
2010-08-09 10:30:05 -0700

    docs for rolling queue.

--- src/jvm/infer/RollingQueue.java
+++ src/jvm/infer/RollingQueue.java
@@ -1,17 +1,12 @@
 package infer;
 
     /// <summary> 
-    /// The IndexableFixedLengthCompoundQueueOfDoubles is an optimized, indexable, fixed-length queue designed to solve 
-    /// two problems for event driven calculations on rolling windows.  First, we discovered 
-    /// performancce issues with a normal queue.  A normal queue is dynamicly resizing ad new 
-    /// enemends are enqued and dequed.  A queue that serves as the data event caching mecahnism 
-    /// to back rolling calculations is used in a specific pattern - data events cause both an 
-    /// enqueue and a dequeue at the same time.  This pattern of useage can cause performace 
-    /// issues for normal queues if it leads to dynamci sizing and resizing.  Since we know the 
-    /// queue is fized length and has a certian useage pattern, we can impliment a different api;
-    /// we can remove the Deque method and foled its behavior into the Enque method.  Moreover, 
-    /// this can be done very efficiently on a fixed length array with a very fast indexing scheme 
-    /// that continues wrapping indexes around the array and overwriting elements that are dequed.
+    /// The Rolling is an optimized, indexable, fixed-length queue for event driven calculations on rolling windows.
+    /// Mormal queues dynamicly resize, but for the rolling update case, data events always cause both an 
+    /// enqueue and a dequeue at the same time.
+    /// We can remove the Deque method and fold its behavior into the Enque method.
+    /// This can be done very efficiently on a fixed length array with an indexing scheme wraps indexes around
+    /// the array and overwrites elements that are dequed.
     /// </summary>
     public class RollingQueue
     {
